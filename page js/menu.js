bd35f370bebd72d38dd0fdaac57830bbae9f87bf
SEKAI-NO-BAKAA <96847864+SEKAI-NO-BAKAA@users.noreply.github.com>
2022-12-23 21:11:33 +0100

    Add files via upload

---
+++ page js/menu.js
@@ -0,0 +1,17 @@
+function toggleMenu () {  
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    
+    burger.addEventListener('click', (e) => {    
+      navbar.classList.toggle('show-nav');
+    });    
+    // bonus
+    const navbarLinks = document.querySelectorAll('.navbar a');
+    navbarLinks.forEach(link => {
+      link.addEventListener('click', (e) => {    
+        navbar.classList.toggle('show-nav');
+      }); 
+    })
+     
+  }
+  toggleMenu();
