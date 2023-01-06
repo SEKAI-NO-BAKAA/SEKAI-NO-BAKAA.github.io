f7bcb4489df8aad2664a3e1b3e36a52b071917b6
SEKAI-NO-BAKAA <96847864+SEKAI-NO-BAKAA@users.noreply.github.com>
2023-01-06 23:30:11 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+    $('.num').counterUp({
+      time: 2400
+    });
+  });
+
+  
+
+  // setTimeout(() => {
+  //   document.querySelector('.preloader').style.display = "none"
+  // }, 4300); 
+
+
+  const loader = document.querySelector('.preloader');
+
+  setTimeout( () => {
+    
+    loader.classList.add('fondu-out');
+    
+    
+
+}, 0000);
+// changer en 4000
+
+
+
