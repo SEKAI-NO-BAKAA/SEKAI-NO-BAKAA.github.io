cca4cee7ac06b972d653090551f4c69e54932b15
SEKAI-NO-BAKAA <96847864+SEKAI-NO-BAKAA@users.noreply.github.com>
2023-01-12 23:17:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -84,47 +84,47 @@ function ouvrirPage()
   
 
  if  ( a == "loli" ||  a==="lolii" ){
-    window.open("../page html/page1.html","_self") ;
+    window.open("page1.html","_self") ;
 }else if  (a==="elle"|| a==="l" || a==="ele"){
-    window.open("../page html/page2.html","_self") ;
+    window.open("page2.html","_self") ;
 }else if  (a === "souvenirs"|| a==="souvenir"|| a==="souvnir") {
-    window.open("../page html/page3.html","_self") ;
+    window.open("page3.html","_self") ;
 }else if  (a==="baka" || a==="bakaa"){
-    window.open("../page html/page4.html","_self") ;
+    window.open("page4.html","_self") ;
 }else if  (a === "personne" || a==="personnes") {
-    window.open("../page html/page5.html","_self") ;
+    window.open("page5.html","_self") ;
 }else if  (a==="oubliemoi" || a==="oubliermoi") {
-    window.open("../page html/page6.html","_self") ;
+    window.open("page6.html","_self") ;
 }else if  ( a==="tuasaimélamort" || a==="t'asaimélamort" ){
-    window.open("../page html/page7.html","_self") ;
+    window.open("page7.html","_self") ;
 }else if  (a === "cendres" || a==="cendre" ){
-    window.open("../page html/page8.html","_self") ;
+    window.open("page8.html","_self") ;
 }else if  (a === "betedefoire" || a==="bêtedefoire" ){
-    window.open("../page html/page9.html","_self") ;
+    window.open("page9.html","_self") ;
 }else if  ( a==="lettre" || a==="lettreaumajor" || a==="lettresaumajor" || a==="lettreauxmajors"){
-    window.open("../page html/page10.html","_self") ;
+    window.open("page10.html","_self") ;
 }else if  ( a==="tagada" || a==="tagadaa") {
-    window.open("../page html/page11 Tagada.html","_self") ;
+    window.open("page11 Tagada.html","_self") ;
 }else if  (a==="mercimajor" ||a==="merci" ){
-    window.open("../page html/page Merci.html","_self") ;
+    window.open("page Merci.html","_self") ;
 }else if  (a==="encoreunefois"|| a==="encoreuneencore" || a==="encore" ){
-	window.open("../page html/page12 Encore une fois.html","_self") ;
+	window.open("page12 Encore une fois.html","_self") ;
 }else if  (a==="fuismoi" || a==="fuis" ){
-	window.open("../page html/page13 Fuis moi.html","_self") ;
+	window.open("page13 Fuis moi.html","_self") ;
 }else if  ( a === "bulle" ||a === "bulles" || a==="bule"){
-	window.open("../page html/page bulle.html","_self") ;
+	window.open("page bulle.html","_self") ;
 }else if  ( a === "nefaitespascommesaki" ||a === "saki" || a===" nefaitepascommesaki"|| a==="177013" ){
-	window.open("../page html/page ne faites pas comme Saki.html","_self") ;
+	window.open("page ne faites pas comme Saki.html","_self") ;
 }else if  ( a === "fautpast'enfaire" ||a === "fautt'enfaire" || a==="fautpas"|| a==="fautpass'enfaire"|| a==="fautpastenfaire"|| a==="fautpassenfaire" ){
-	window.open("../page html/page faut pas t'en faire.html","_self") ;
+	window.open("page faut pas t'en faire.html","_self") ;
 }else if  ( a === "destin" ||a === "desstin" || a==="destiné"){
-	window.open("../page html/page destin.html","_self") ;
+	window.open("page destin.html","_self") ;
 }else if  ( a === "lejeuduloup" ||a === "lejeuxduloup" || a==="jeuduloup"){
-	window.open("../page html/page jeu du loup.html","_self") ;
+	window.open("page jeu du loup.html","_self") ;
 }else if  ( a === "happyend" ||a === "hapyend" || a==="happy"){
-	window.open("../page html/page happy end.html","_self") ;
+	window.open("page happy end.html","_self") ;
 }else if  ( a === "coeurdanslesabysses"||a === "coeurdanslesabysse" || a==="coeur"|| a==="abysse"|| a==="abysses"|| a==="madeinabyss"|| a==="madeinabyyse" ){
-	window.open("../page html/page abysse.html","_self") ;
+	window.open("page abysse.html","_self") ;
 }
 	
 	
